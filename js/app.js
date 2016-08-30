b8bc63c8c308d0924ed9426713cd497c66a94338
Sheen Joseph <sheen.joseph@emc.com>
2016-08-30 15:48:33 -0600

    Front end changes

--- js/app.js
+++ js/app.js
@@ -13,6 +13,52 @@ angular.module('app', ['ngMessages','ui.bootstrap','ngFileUpload'])
 
         $scope.categoryFilter = '';
 
+        $scope.items = [{
+            id: 1,
+            title: 'Alaskan Reindeer',
+            desc: 'Smoky, juicy and delicious. So good we flew it all the way from Alaska!',
+            comments: []
+        }, {
+            id: 2,
+            title: 'California Reindeer',
+            desc: '',
+            comments: []
+        }, {
+            id: 3,
+            title: 'Arkansas Reindeer',
+            desc: '',
+            comments: []
+        }, {
+            id: 4,
+            title: 'Mainedeer',
+            desc: '',
+            comments: []
+        }, {
+            id: 5,
+            title: 'Texas Roadkill',
+            desc: '',
+            comments: []
+        }, ];
+        //$scope.active = $scope.items[0];
+        $scope.toggleActiveItem = function(item) {
+            if ($scope.active === item) {
+                $scope.active = false;
+            } else {
+                $scope.active = item;
+            }
+            console.log($scope.active);
+        };
+        $scope.showCommentPanel = false;
+        $scope.toggleCommentPanel = function() {
+            $scope.showCommentPanel = !$scope.showCommentPanel;
+        }
+
+        $scope.newComment="";
+        $scope.saveComment = function(item,newComment){
+            item.comments.push(newComment);
+            $scope.newComment="";
+        }
+
         $scope.notifyUsers = function () {
             console.log('notifyUsers');
         };
@@ -21,12 +67,14 @@ angular.module('app', ['ngMessages','ui.bootstrap','ngFileUpload'])
             $scope.notifyUsers();
         });
         $scope.search = function (artifacts) {
-            console.log("............"+artifacts.description);
             if($scope.query != null){
                 return !!((artifacts.description.indexOf($scope.query || '') !== -1 || artifacts.title.indexOf($scope.query || '') !== -1));
             }
         };
-
+        $scope.resetFilter = function(){
+            console.log('resetFilter');
+            $scope.ddValue = '';
+        }
         $scope.filterArtifact = function (data) {
             $scope.getData('artifacts', {category: data});
 
@@ -60,12 +108,12 @@ angular.module('app', ['ngMessages','ui.bootstrap','ngFileUpload'])
             $http.post('/madeit/artifacts', artifact).then(function(response) {
                 console.log('addArtifact good', response);
                 $scope.artifacts.push(artifact);
-                // artifact.category = '';
-                // artifact.description = '';
-                // artifact.title = '';
-                // artifact.asset = '';
-                // artifact.comments = '';
-                // artifact.creator = '';
+                artifact.category = '';
+                artifact.description = '';
+                artifact.title = '';
+                artifact.asset = '';
+                artifact.comments = '';
+                artifact.creator = '';
             }, function(err) {
                 console.log('addArtifact err', err);
             });
