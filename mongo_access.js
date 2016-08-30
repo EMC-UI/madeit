1f90d1de8a4eb1e0e07d91ea3d2fd3b8e1930534
coboulderer <rick.kowaleski@gmail.com>
2016-08-30 11:16:02 -0600

    updated mongo access

--- mongo_access.js
+++ mongo_access.js
@@ -1,24 +1,35 @@
 var mongodb = require('mongodb');
 var fs = require('fs');
 var Grid = require('gridfs');
+var dbName = 'caveman';
+var defaultDBConnection = `mongodb://128.222.174.194/${dbName}`;
 
-var MongoDataAccess = function () {
-
-    var dbName = 'caveman';
-    var defaultDBConnection = `mongodb://128.222.174.194/${dbName}`;
-
-    this.writeFileToMongo = function (filePath) {
-        mongodb.MongoClient.connect(defaultDBConnection, function(err, db) {
-            var pos = filePath.lastIndexOf("/");
-            var fileName = filePath.substring(pos);
-            var gfs = Grid(db, mongodb);
-            gfs.fromFile({filename: fileName}, filePath, function (err, file) {
-                console.log('saved %s to GridFS file %s', filePath, file.filename);
-            });
+var writeFileToMongo = function (filePath) {
+    mongodb.MongoClient.connect(defaultDBConnection, function(err, db) {
+        var pos = filePath.lastIndexOf("/");
+        var fileName = filePath.substring(pos);
+        var gfs = Grid(db, mongodb);
+        gfs.fromFile({filename: fileName}, filePath, function (err, file) {
+            console.log('saved %s to GridFS file %s', filePath, file.filename);
         });
-    };
+    });
+};
 
-    this.readFromMongo = function () {
+var getListOfMongoFiles = function(callback) {
+    var fileNames = [];
+    mongodb.MongoClient.connect(defaultDBConnection, function (err, db) {
+        var gfs = Grid(db, mongodb);
+        gfs.list(function (err, files) {
+            files.forEach(function(filename) {
+                //console.log("file name found = " + filename);
+                fileNames.push(filename);
+            });
+            callback(fileNames);
+        })
+    });
+};
+
+var getFileFromMongo = function (fileName) {
     //     mongoClient.connect(dbURI).then(ddb => {
     //         console.log('connected to mongo');
     //     db = ddb;
@@ -27,17 +38,17 @@ var MongoDataAccess = function () {
     //
     //     //iterate through cursor and return files....
     // })
-    };
 };
 
 
-    // this.readFilesFromMongo = function () {
-    //     mongoClient.connect(dbURI).then(ddb => {
-    //         console.log('connected to mongo');
-    //     db = ddb;
-    //     var bucket = new mongodb.GridFSBucket(db, { bucketName: bucketName });
-    // }
-    // };
+
+// this.readFilesFromMongo = function () {
+//     mongoClient.connect(dbURI).then(ddb => {
+//         console.log('connected to mongo');
+//     db = ddb;
+//     var bucket = new mongodb.GridFSBucket(db, { bucketName: bucketName });
+// }
+// };
 
 
 
@@ -59,11 +70,14 @@ var MongoDataAccess = function () {
 
 
 
-    //     }).catch(er => {
-    //         console.log('error connecting to mongo', er);
-    // });
+//     }).catch(er => {
+//         console.log('error connecting to mongo', er);
+// });
 //     //
 //
 // };
 //
-// module.exports = MongoDataAccess;
+module.exports = {
+    writeFileToMongo: writeFileToMongo,
+    getListOfMongoFiles: getListOfMongoFiles
+}
